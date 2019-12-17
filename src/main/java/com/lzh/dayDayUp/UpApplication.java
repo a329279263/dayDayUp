ea4411a1239e11e6eda49b6a234c1e25aa0186bf
lzh <LZH329279263@163.com>
2019-12-17 19:06:31 +0800

    init

--- src/main/java/com/lzh/dayDayUp/UpApplication.java
+++ src/main/java/com/lzh/dayDayUp/UpApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UpApplication {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         SpringApplication.run(UpApplication.class, args);
     }
 
